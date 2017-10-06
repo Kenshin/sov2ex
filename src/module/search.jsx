667658f19b0de2f0ab87c9f1a87cb5ea740b86e5
Kenshin <fxblog@gmail.com>
2017-10-06 11:41:12 +0800

    Add search page top style.

--- src/module/search.jsx
+++ src/module/search.jsx
@@ -1,10 +1,30 @@
 console.log( "==== sov2ex module: Search ====" )
 
+import TextField from 'textfield';
+
 export default class Search extends React.Component{
     
     render() {
         return (
-            <div> Search </div>
+            <div className="searchpage">
+                <div className="top">
+                    <div className="logo">
+                        <img src="./assets/images/logo@1x.png"></img>
+                    </div>
+                    <div className="searchbar">
+                        <div className="search">
+                            <TextField 
+                                ref="search"
+                                placeholder="请输入查询的关键字" 
+                            />
+                            <span className="bar" onClick={ ()=> this.onClick() }></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="searchresults">
+                
+                </div>
+        </div>
         )
     }
 }
