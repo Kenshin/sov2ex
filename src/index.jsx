36bb7049f40c0e91b74b3b26ce0da258a1c19b6a
Kenshin <fxblog@gmail.com>
2017-10-09 11:48:12 +0800

    Add notify style.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 console.log( "==== sov2ex Index ====" )
 
 import './assets/css/style.css';
+import './vender/notify/notify.css';
 
 import Entry      from 'entry';
 import Search     from 'search';
